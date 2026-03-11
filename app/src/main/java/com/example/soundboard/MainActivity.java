37b76e2042e64f64800381884487617437ff14c1
Mateusz Ostróżka <mostrozka@technikumlodz.pl>
2026-03-11 14:04:36 +0100

    Soundboard

---
+++ app/src/main/java/com/example/soundboard/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.soundboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnPlaySound;
+    private VideoView videoView;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPlaySound = findViewById(R.id.btnPlaySound);
+        videoView = findViewById(R.id.videoView);
+
+        btnPlaySound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer != null) {
+                    mediaPlayer.release();
+                }
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.sample_music);
+                if (mediaPlayer != null) {
+                    mediaPlayer.start();
+                }
+            }
+        });
+
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.sample_video;
+        Uri uri = Uri.parse(videoPath);
+
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+}
